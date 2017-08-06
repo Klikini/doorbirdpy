e4459c62861fa78414d34da10012ce05df313ba2
Andy Castille <klikini@robiotic.net>
2017-08-05 19:42:38 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="DoorBirdPy",
-    version="0.0.2",
+    version="0.0.4",
     author="Andy Castille",
     author_email="andy@robiotic.net",
     packages=["doorbirdpy"],
